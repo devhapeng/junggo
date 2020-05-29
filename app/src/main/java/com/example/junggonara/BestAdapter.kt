16177ea48f7165fdd7980539a6904f7f3bcf16de
devhapeng <hayoung8602@gmail.com>
2020-05-30 02:17:56 +0900

    second commit

---
+++ app/src/main/java/com/example/junggonara/BestAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.junggonara
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BestAdapter(private val context : Context) : RecyclerView.Adapter<BestViewHolder>() {
+    var datas = listOf<BestData>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BestViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_best, parent, false)
+        return BestViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    override fun onBindViewHolder(holder: BestViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+}
--- app/src/main/java/com/example/junggonara/SearchActivity.kt
+++ app/src/main/java/com/example/junggonara/SearchActivity.kt
@@ -1,15 +1,31 @@
 package com.example.junggonara
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.search_activity.*
 
 class SearchActivity : AppCompatActivity() {
 
+    lateinit var rvBestClass : RecyclerView
+    lateinit var rvbestAdapter: BestAdapter
+    val datas = mutableListOf<BestData>()
+
+    lateinit var rvMustClass : RecyclerView
+    lateinit var rvMustAdapter : MustAdapter
+    val datas1 = mutableListOf<MustData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_activity)
+
+        initBestItemRecyclerView()
+        initMustItemRecyclerView()
 
         allviewer.setOnClickListener {
             showToast("전체보기가 클릭되었습니다.")
@@ -20,9 +36,9 @@ class SearchActivity : AppCompatActivity() {
             
             //인증 상품은 눌린채로, 나머지는 non으로
             certification.setImageResource(R.drawable.search_btn_certification)
-//            star_5.setImageResource(R.drawable.search_btn_5_star_non)
-//            star_5.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.search_btn_5_star_non))
-//            notopen.setImageResource(R.drawable.search_btn_notopen_non)
+            star_5.setImageResource(R.drawable.search_btn_5_star_non)
+            star_5.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.search_btn_5_star_non))
+            notopen.setImageResource(R.drawable.search_btn_notopen_non)
         }
 
         star_5.setOnClickListener {
@@ -32,8 +48,8 @@ class SearchActivity : AppCompatActivity() {
             //5성급 후기는 눌린채로, 나머지는 non으로
 
             certification.setImageResource(R.drawable.search_btn_certification_non)
-//            star_5.setImageResource(R.drawable.search_btn_5star)
-//            notopen.setImageResource(R.drawable.search_btn_notopen_non)
+            star_5.setImageResource(R.drawable.search_btn_5star)
+            notopen.setImageResource(R.drawable.search_btn_notopen_non)
 
         }
 
@@ -44,9 +60,109 @@ class SearchActivity : AppCompatActivity() {
             //미개봉상품은 눌린채로, 나머지는 non으로
 
             certification.setImageResource(R.drawable.search_btn_certification_non)
-//            star_5.setImageResource(R.drawable.search_btn_5_star_non)
-//            notopen.setImageResource(R.drawable.search_btn_notopen)
+            star_5.setImageResource(R.drawable.search_btn_5_star_non)
+            notopen.setImageResource(R.drawable.search_btn_notopen)
 
         }
     }
+
+    fun initBestItemRecyclerView(){
+        rvBestClass = findViewById(R.id.bestview)
+        rvMustClass = findViewById(R.id.product)
+
+        rvbestAdapter = BestAdapter(this)
+        rvMustAdapter = MustAdapter(this)
+
+        rvBestClass.adapter = rvbestAdapter
+        rvMustClass.adapter = rvMustAdapter
+
+        rvBestClass.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvMustClass.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        rvbestAdapter.datas = listOf(
+            BestData(
+                image_best = R.drawable.search_img_user1,
+                userName = "솝트",
+                textLevel = "실버 Lv.4",
+                medal = R.drawable.search_ic_silver
+            ),
+            BestData(
+                image_best = R.drawable.search_img_user2,
+                userName = "스타벅스",
+                textLevel = "브론즈 Lv.3",
+                medal = R.drawable.search_ic_bronze
+            ),
+            BestData(
+                image_best = R.drawable.search_img_user3,
+                userName = "엔젤리너스",
+                textLevel = "골드 Lv.1",
+                medal = R.drawable.search_ic_gold
+            ),
+            BestData(
+                image_best = R.drawable.search_img_user4,
+                userName = "SOPT",
+                textLevel = "실버 Lv.2",
+                medal = R.drawable.search_ic_silver1
+            )
+        )
+
+
+        rvbestAdapter.notifyDataSetChanged()
+    }
+
+
+    fun initMustItemRecyclerView(){
+        rvMustClass = findViewById(R.id.product)
+
+        rvMustAdapter = MustAdapter(this)
+
+        rvMustClass.adapter = rvMustAdapter
+
+        rvMustClass.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        rvMustAdapter.datas1 = listOf(
+            MustData(
+                product_image = R.drawable.search_img_product1,
+                cer_mini = R.drawable.search_img_certification1,
+                new_mini = R.drawable.search_img_new1,
+                product_name = "여성 엘칸토 로퍼 250",
+                price = "45,000원"
+            ),
+            MustData(
+                product_image = R.drawable.search_img_product2,
+                cer_mini = R.drawable.search_img_certification1,
+                new_mini = null,
+                product_name = "[패션] 여성용 팬츠 자켓 가디건 봄 신상\n" +
+                        "균일가 F(95) 무료배송",
+                price = "45,000원"
+            ),
+            MustData(
+                product_image = R.drawable.search_img_product3,
+                cer_mini = R.drawable.search_img_certification1,
+                new_mini = R.drawable.search_img_new1,
+                product_name = "여성 엘칸토 로퍼 250",
+                price = "45,000원"
+            ),
+            MustData(
+                product_image = R.drawable.search_img_product4,
+                cer_mini = R.drawable.search_img_certification1,
+                new_mini = null,
+                product_name = "[패션] 여성용 팬츠 자켓 가디건 봄 신상\n" +
+                        "균일가 F(95) 무료배송",
+                price = "45,000원"
+            ),
+            MustData(
+                product_image = R.drawable.search_img_product5,
+                cer_mini = R.drawable.search_img_certification1,
+                new_mini = null,
+                product_name = "여성 엘칸토 로퍼 250",
+                price = "45,000원"
+            )
+        )
+
+        rvMustAdapter.notifyDataSetChanged()
+    }
+
 }
+
+
